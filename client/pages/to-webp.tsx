613d371f7b5cd3f41749dbde1bbf04378920c471
osawa-koki <github@osawa.dev>
2023-01-04 20:37:09 +0000

    canvas

--- client/pages/to-webp.tsx
+++ client/pages/to-webp.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Button, Alert, Form } from 'react-bootstrap';
 import Layout from "../components/Layout";
@@ -7,14 +7,53 @@ export default function ToWebP() {
 
   const [file, setFile] = useState<File | null>(null);
 
+  useEffect(() => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const target = e.target as FileReader;
+        const result = target.result;
+        if (result) {
+          const img = new Image();
+          img.src = result;
+          img.onload = () => {
+            const canvas = document.createElement("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext("2d");
+            if (ctx) {
+              ctx.drawImage(img, 0, 0);
+              const dataURL = canvas.toDataURL("image/webp");
+              const blob = dataURLtoBlob(dataURL);
+              const file = new File([blob], "test.webp", { type: "image/webp" });
+              console.log(file);
+            }
+          };
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  }, [file]);
+
+  const FileLoad = (e: React.FormEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    const files = target.files;
+    if (files) {
+      setFile(files[0]);
+    }
+  };
+
   return (
     <Layout>
       <main>
         <div>
           <Form.Group>
             <Form.Label>Put Image file.</Form.Label>
-            <Form.Control type="file" />
+            <Form.Control onInput={FileLoad} type="file" />
           </Form.Group>
+        </div>
+        <div id="MyCanvasDiv">
+          <canvas id="MyCanvas"></canvas>
         </div>
       </main>
     </Layout>
