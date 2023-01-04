bfc46f661573c6293548deae52b5eae6638c73e5
osawa-koki <github@osawa.dev>
2023-01-04 20:39:43 +0000

    処理大枠部分。

--- client/pages/to-webp.tsx
+++ client/pages/to-webp.tsx
@@ -8,31 +8,28 @@ export default function ToWebP() {
   const [file, setFile] = useState<File | null>(null);
 
   useEffect(() => {
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const target = e.target as FileReader;
-        const result = target.result;
-        if (result) {
-          const img = new Image();
-          img.src = result;
-          img.onload = () => {
-            const canvas = document.createElement("canvas");
-            canvas.width = img.width;
-            canvas.height = img.height;
-            const ctx = canvas.getContext("2d");
-            if (ctx) {
-              ctx.drawImage(img, 0, 0);
-              const dataURL = canvas.toDataURL("image/webp");
-              const blob = dataURLtoBlob(dataURL);
-              const file = new File([blob], "test.webp", { type: "image/webp" });
-              console.log(file);
-            }
-          };
-        }
-      };
-      reader.readAsDataURL(file);
-    }
+    if (!file) return;
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const target = e.target as FileReader;
+      const result = target.result;
+      if (!result) return;
+      const img = new Image();
+      img.src = result;
+      img.onload = () => {
+        const canvas = document.getElementById("MyCanvas") as HTMLCanvasElement;
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext("2d");
+        if (!ctx) return;
+        ctx.drawImage(img, 0, 0);
+        const dataURL = canvas.toDataURL("image/webp");
+        const blob = dataURLtoBlob(dataURL);
+        const file = new File([blob], "test.webp", { type: "image/webp" });
+        console.log(file);
+      }
+    };
+    reader.readAsDataURL(file);
   }, [file]);
 
   const FileLoad = (e: React.FormEvent<HTMLInputElement>) => {
